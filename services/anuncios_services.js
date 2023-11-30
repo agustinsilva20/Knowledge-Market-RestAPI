a19232a80216f5283b01b0e6a88815137d41260c
agustinsilva20 <agustinsilvab@hotmail.com>
2023-11-29 23:27:45 -0300

    listar en el perfil

--- services/anuncios_services.js
+++ services/anuncios_services.js
@@ -66,7 +66,7 @@ exports.getAnuncios = async function () {
 
 exports.getAnunciosByProfesor = async function (profesorID) {
 
-    const query = `SELECT * FROM Curso WHERE CURSO.PROFESORID = ${profesorID}`;
+    const query = `SELECT * FROM Curso WHERE Curso.PROFESORID = ${profesorID} And Curso.Estado != "ELIMINADO" `;
     const result = await db.run_query(query)
     return {"msg": result, error: null};
 }
